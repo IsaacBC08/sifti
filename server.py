e3879817313a4b568838d3d7ad8056910365ec03
Isaac <bolanosisaac45@gmail.com>
2024-06-05 20:42:33 -0600

    Horarios completos (3/3)

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import os
 import json
 
 
-PORT = 8222
+PORT = 10000
 PASSWORD = "sifti4321"
 STATIC_DIR = os.path.join(os.path.dirname(__file__), 'static')
 ANUNCIOS_JSON_FILE = os.path.join(os.path.dirname(__file__), 'static/db/anuncios.json')

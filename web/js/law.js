6d7dc5a65501df650bb2547f4346d3f0c832705d
Isaac <bolanosisaac45@gmail.com>
2024-06-23 22:25:49 -0600

    mejoras al diseño del chatbot

--- web/js/law.js
+++ web/js/law.js
@@ -4,9 +4,19 @@ document.getElementById('commandForm').addEventListener('submit', async function
     const command = document.getElementById('commandInput').value;
     const responseOutput = document.getElementById('responseOutput');
 
+    // Añadir el mensaje del usuario
     const userMessageDiv = document.createElement('div');
     userMessageDiv.classList.add('chat-message', 'user');
-    userMessageDiv.textContent = command;
+
+    const userImage = document.createElement('img');
+    userImage.src = '../img/user.png'; // Reemplaza con la ruta de la imagen del usuario
+
+    const userMessageContent = document.createElement('div');
+    userMessageContent.classList.add('message-content');
+    userMessageContent.textContent = command;
+
+    userMessageDiv.appendChild(userImage);
+    userMessageDiv.appendChild(userMessageContent);
     responseOutput.appendChild(userMessageDiv);
     responseOutput.scrollTop = responseOutput.scrollHeight;
 
@@ -21,41 +31,45 @@ document.getElementById('commandForm').addEventListener('submit', async function
 
         const data = await response.json();
 
+        // Añadir el mensaje del bot
         const botMessageDiv = document.createElement('div');
         botMessageDiv.classList.add('chat-message', 'law');
 
-        const botMessageText = document.createElement('span');
-        botMessageText.classList.add('typewriter-text');
+        const botImage = document.createElement('img');
+        botImage.src = '../img/lawliett.png'; // Reemplaza con la ruta de la imagen del bot
 
-        respuesta = data.response;
-        botMessageDiv.appendChild(botMessageText);
+        const botMessageContent = document.createElement('div');
+        botMessageContent.classList.add('message-content');
+
+        botMessageDiv.appendChild(botImage);
+        botMessageDiv.appendChild(botMessageContent);
         responseOutput.appendChild(botMessageDiv);
         responseOutput.scrollTop = responseOutput.scrollHeight;
         document.getElementById('commandInput').value = '';
-        // Función para simular el efecto de escritura
-        async function typeWriterEffect(text) {
-            for (let i = 0; i < text.length; i++) {
-                botMessageText.textContent += text.charAt(i);
-                await sleep(25); // Esperar 0.1 segundos antes de añadir el siguiente carácter
-            }
-        }
 
-        // Llamar a la función para mostrar el mensaje letra por letra
-        await typeWriterEffect(respuesta);
-
-        function sleep(ms) {
-            return new Promise(resolve => setTimeout(resolve, ms));
-        }
-
-
+        await typeWriterEffect(data.response, botMessageContent);
 
     } catch (error) {
         const errorMessageDiv = document.createElement('div');
         errorMessageDiv.classList.add('chat-message');
-        errorMessageDiv.textContent = `Error: ${error}`;
+
+        const errorMessageContent = document.createElement('div');
+        errorMessageContent.classList.add('message-content');
+        errorMessageContent.textContent = `Error: ${error}`;
+
+        errorMessageDiv.appendChild(errorMessageContent);
         responseOutput.appendChild(errorMessageDiv);
         responseOutput.scrollTop = responseOutput.scrollHeight;
     }
-
-
 });
+
+async function typeWriterEffect(text, element) {
+    for (let i = 0; i < text.length; i++) {
+        element.textContent += text.charAt(i);
+        await sleep(25);
+    }
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
